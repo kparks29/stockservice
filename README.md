f2e6453b5c66d78ceffb6e839179316a8d8d91cc
Kendrick Parks <kendrick@homdna.com>
2015-08-21 11:04:03 -0700

    create install script (needs some tweaking) create migration script runner

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# stockservice
+Requirements
+
+ - npm install
+ - node installed
+
+Run the following commands in your terminal
+
+ chmod 777 install.sh
+ ./install.sh
+
+Open another tab and run
+
+ node app.js
+
+After running those commands your local server and db should be up and running
--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-var pg = require('pg');
+var pg = require('pg'),
+    dbConfig = require('./db.conf.json'),
+    conString = 'postgres://' + dbConfig.username + ':' + dbConfig.password + '@localhost/' + dbConfig.db + '',
+    client = new pg.Client(conString)
+    migrations = require('./migrations.json').migrations;
 
-var conString = "postgres://kendrick:homdna@localhost/stockservice";
-
-var client = new pg.Client(conString);
 client.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);
   }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
-  });
+  function queryDb(i) {
+    client.query(migrations[i], function(err, result) {
+      if(err) {
+        console.error('error running query', err);
+      }
+      else {
+        console.log('Ran migration ' + (i + 1));
+      }
+      client.end();
+    });
+  }
+  for (var i=0; i<migrations.length; i++) {
+    queryDb(i);
+  }
 });
