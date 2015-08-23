49056ffd509b58380e4f125841a2bedcbca868c1
Kendrick Parks <kendrick@homdna.com>
2015-08-22 20:16:54 -0700

    add promise to db

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-var pg = require('pg'),
+var pg = require('pg-promise')(),
     dbConfig = require('./db.conf.json'),
     conString = 'postgres://' + dbConfig.username + ':' + dbConfig.password + '@localhost/' + dbConfig.db + '',
-    client = new pg.Client(conString)
-    migrations = require('./db/migrations.json').migrations;
+    db = pg(conString),
+    migrations = require('./db/migrations.json').migrations,
+    userController = require('./users/userController'),
+    Promise = require('promise');
 
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  function queryDb(i) {
-    client.query(migrations[i], function(err, result) {
-      if(err) {
-        console.error('error running query', err);
-      }
-      else {
-        console.log('Ran migration ' + (i + 1));
-      }
-      client.end();
-    });
-  }
+function errorHandler(error) {
+  console.log('ERROR', error)
+}
+
+// migrate db
+db.tx(function(){
+  var promises = [];
   for (var i=0; i<migrations.length; i++) {
-    queryDb(i);
+    promises.push(this.none(migrations[i]));
   }
-});
+  return Promise.all(promises).then(function() {
+    console.log('Migrations Complete');
+  });
+}).then(function(){
+  // close the connection to db
+  pg.end();
+  // load user module 
+  userController.test();
+}, errorHandler);
+
