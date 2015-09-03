1bb5ebfc24a0c411e9d4a5ade28b7010a08a504b
Kendrick Parks <kendrick@homdna.com>
2015-09-03 08:20:38 -0700

    login works with real users on local db

--- auth/auth.js
+++ auth/auth.js
@@ -1,7 +1,18 @@
 (function() {
 	'use strict';
 	var express = require('express'),
-		router = express.Router();
+		router = express.Router(),
+		bcrypt = require('bcrypt'),
+		_ = require('lodash'),
+		queries = require('../db/queries.json'),
+		pg = require('pg-promise')(),
+        dbConfig = require('../db.conf.json'),
+        conString = 'postgres://' + dbConfig.username + ':' + dbConfig.password + '@localhost/' + dbConfig.db + '',
+        db = pg(conString);
+
+	function getUser (email_address) {
+		return db.one(queries.users.getByEmailAddress, [email_address]);
+	}
 
 	router.use(function timeLog(req, res, next) {
 		console.log('Request made at: ', Date.now());
@@ -11,14 +22,28 @@
 		next();
 	});
 	router.post('/login', function(req, res) {
-		if (req.body.email && req.body.password) {
-			res.json({
-				auth_token: 'test'
+		if (req.body.email_address && req.body.password) {
+			getUser(req.body.email_address).then(function (user) {
+				if (bcrypt.compareSync(req.body.password, user.hashed_password)) {
+					res.status(200).json({
+						auth_token: 'test'
+					});
+				}
+				else {
+					res.status(401).json({
+						error: 'Wrong email or password'
+					});
+				}
+			}).catch(function (error) {
+				res.status(500).json({
+					error: 'Internal Server Error'
+				});
 			});
+			
 		}
 		else {
-			res.status(401).json({
-				error: 'Wrong email or password'
+			res.status(400).json({
+				error: 'Missing Email or Password'
 			});
 		}
 	});
